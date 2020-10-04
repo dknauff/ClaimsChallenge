128983545a2100002ab6431c3b4b19bf3856d499
dknauff <dknauffsd@gmail.com>
2020-10-03 22:51:02 -0400

    Saturday Night

--- Claims_Project_Tests/ClaimTests.cs
+++ Claims_Project_Tests/ClaimTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Claims_Project_Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class ClaimTests
     {
         [TestMethod]
         public void TestMethod1()
